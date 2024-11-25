bf8b08d8402625f454134ae287cee1592ace7753
Andreasafent <afentoulidisandreas@gmail.com>
2024-11-25 21:41:42 +0200

    Added posts. Added Post controller.

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+    
+    <div class="row">
+        <div class="col-12">
+            <h1>{{ $post->title }}</h1>
+            <p class="text-muted">Published at: {{$post->created_at->format('d/m/y')}}</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 text-center">
+            <img src="{{ $post->image}}" width="400" class="img-thumbnail">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <p>{{ $post->description }}</p>
+            <br><br>
+            By {{ $post->author}}
+        </div>
+    </div>
+</div>
+
+@stop
