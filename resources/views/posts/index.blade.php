750f2f5d2be30aacc9dd94bea98f1e495976b569
Andreasafent <afentoulidisandreas@gmail.com>
2024-12-02 18:53:45 +0200

    Finalize Post C.R.U.D.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,11 +5,17 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <h1>Posts</h1>
+            <div class="d-flex">
+                <h1 class="flex-grow-1">Posts</h1>
+                <div class="">
+                    <a href="/posts/create" class="btn btn-primary">Create New Post</a>
+                </div>
+            </div>
         </div>
     </div>
 
     <div class="row">
+
         @foreach ($posts as $post)
             <div class="col-4">
                 <div class="card" style="width: 18rem;">
@@ -18,10 +24,13 @@
                         <h5 class="card-title">{{$post->title}}</h5>
                         <p class="card-text">{{$post->intro}}</p>
                         <a href="/posts/{{$post->id}}" class="btn btn-primary">Read now</a>
+                        <a href="/posts/{{$post->id}}/delete" class="btn btn-danger">X</a>
+                        <a href="/posts/{{$post->id}}/edit" class="btn btn-warning">✏️</a>
                     </div>
                 </div>
             </div>  
         @endforeach
+
     </div>
 </div>
 
